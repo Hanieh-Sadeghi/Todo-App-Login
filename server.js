ec6523ce22a1e9f2a12dd4a5da811d6ea2cc4571
Hanieh-Sadeghi <haniehsadeghi2@outlook.com>
2023-10-28 16:50:22 +0330

    add passport config

--- server.js
+++ server.js
@@ -1,31 +1,89 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 const express = require("express");
 const app = express();
-const bcrypt = require('bcrypt')
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const flash = require("express-flash");
+const session = require("express-session");
+const methodOverride = require('method-override')
 
-const users = []
+const initializePassport = require("./passport-config");
+initializePassport(
+  passport,
+  (email) => users.find((user) => user.email === email),
+  (id) => users.find((user) => user.id === id)
+);
+const users = [];
 
 app.set("view-engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
-// app.use(flash())
+app.use(flash());
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(methodOverride('_method '))
 
-app.get("/", (req, res) => {
-  res.render("index.ejs", { name: "hanieh" });
+app.get("/", checkNotAuthenticated , (req, res) => {
+  res.render("index.ejs", { username: 'hanie' }); //req.user.username
 });
 
-app.get("/login", (req, res) => {
+app.get("/login", checkNotAuthenticated , (req, res) => {
   res.render("login.ejs");
 });
 
-app.post("/login", (req, res) => {
-  
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: true
+}))
+
+app.get('/register', checkNotAuthenticated, (req, res) => {
+  res.render('register.ejs')
+})
+
+
+app.post("/register", async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    users.push({
+      id: Date.now().toString(),
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+    res.redirect("/login");
+  } catch {
+    res.redirect("/register");
+  }
 });
 
-app.get("/register", (req, res) => {
-  res.render("register.ejs");
-});
 
-app.post("/register", (req, res) => {
+app.delete('/logout', (req, res) => {
+  req.logOut()
+  res.redirect('/login')
+})
 
-});
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next()
+  }
 
-app.listen(3000);
+  res.redirect('/login')
+}
+
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect('/')
+  }
+  next()
+}
+
+app.listen(3000)
