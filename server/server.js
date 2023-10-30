2c99189ea1aef2b24b45248897c765cf34447a8d
Hanieh-Sadeghi <haniehsadeghi2@outlook.com>
2023-10-30 23:10:06 +0330

    add serve

---
+++ server/server.js
@@ -0,0 +1,116 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const express = require("express");
+const app = express();
+const PORT = 3000;
+const router = require("./router");
+
+const bodyParser = require("body-parser");
+
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const flash = require("express-flash");
+const session = require("express-session");
+const methodOverride = require("method-override");
+const path = require("path");
+
+const users = [];
+
+app.use(express.json());
+app.use("/v1/api", router);
+app.use(express.static(path.join(__dirname, "./views")));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const initializePassport = require("../passport-config");
+initializePassport(
+  passport,
+  email => users.find((user) => user.email === email),
+  id => users.find((user) => user.id === id)
+);
+
+app.set("view-engine", "ejs");
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: false }));
+app.use(flash());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(methodOverride("_method "));
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.get("/", checkNotAuthenticated, (req, res) => {
+  res.render("index.ejs"); //req.user.username
+});
+
+app.get("/login", checkNotAuthenticated, (req, res) => {
+  res.render("login.ejs");
+});
+
+app.post(
+  "/login",
+  checkNotAuthenticated,
+  passport.authenticate("local", {
+    successRedirect: "/todo",
+    failureRedirect: "/login",
+    failureFlash: true,
+  })
+);
+
+app.get("/register", checkNotAuthenticated, (req, res) => {
+  res.render("register.ejs");
+});
+
+app.post('/register', checkNotAuthenticated, async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    users.push({
+      id: Date.now().toString(),
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword
+    })
+    res.redirect('/login')
+  } catch {
+    res.redirect('/register')
+  }
+  console.log(users)
+});
+
+app.get("/todo", checkAuthenticated ,(req, res) => {
+  res.render("todo.ejs", { username: req.user.username }); 
+});
+
+app.delete("/logout", (req, res) => {
+  req.logOut();
+  res.redirect("/login");
+});
+
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next()
+  }
+
+  res.redirect('/login')
+}
+
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect('/todo')
+  }
+  next()
+}
+
+
+app.listen(3000, () => {
+  console.log(`listening pn port ${PORT}`);
+});
